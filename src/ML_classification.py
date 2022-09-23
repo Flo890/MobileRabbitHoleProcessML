35ec6e3af4026632fb5bcdc1d9013988bdc4729b
florianb <florian.bemmann@ifi.lmu.de>
2022-09-23 10:28:46 +0200

    relative filepaths for GPU sever

--- src/ML_classification.py
+++ src/ML_classification.py
@@ -382,7 +382,7 @@ if __name__ == '__main__':
 
     report_all = pd.DataFrame()
 
-    path = fr'C:\projects\rabbithole\RabbitHoleProcess\data\dataframes\sessiongroups-ml\labled_data\user-sessions_features_all_labled_more_than_intention_normal_age_no_esm_no_personal.pickle'
+    path = fr'../../RabbitHoleProcess/data/dataframes/sessiongroups-ml/labled_data/user-sessions_features_all_labled_more_than_intention_normal_age_no_esm_no_personal.pickle'
     # path = fr'{dataframe_dir_ml_labeled}\user-sessions_features_all_labled_more_than_intention_normal_age_no_esm.pickle'
 
     print(f'###################  target: {path}   #############################')  # , file=f)
@@ -410,6 +410,6 @@ if __name__ == '__main__':
 
   #  report_all = svm_classifier(x, y, filename, report_all)
 
-    report_all.to_csv(fr'C:\projects\rabbithole\RabbitHoleProcess\data\dataframes\sessiongroups-ml\report_ml_undersampling_combined_test_no_personal_more1.csv')
+    report_all.to_csv(fr'../../RabbitHoleProcess/data/dataframes/sessiongroups-ml/report_ml_undersampling_combined_test_no_personal_more1.csv')
 
     print('ML done.')
